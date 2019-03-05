e61f6c0a4b4b175b6e44f4629bb546d42e6c6599
Natan Tadesse <natan.tadesse@gmail.com>
2019-03-05 19:06:26 +0300

    ipn model modified

--- YenePaySDK/YenePaySDK/IPNModel.cs
+++ YenePaySDK/YenePaySDK/IPNModel.cs
@@ -19,16 +19,6 @@ namespace YenePaySdk
         /// <value>The buyer identifier.</value>
         public Guid BuyerId { get; set; }
         /// <summary>
-        /// Gets or sets the name of the buyer.
-        /// </summary>
-        /// <value>The name of the buyer.</value>
-        public string BuyerName { get; set; }
-        /// <summary>
-        /// Gets or sets the transaction fee.
-        /// </summary>
-        /// <value>The transaction fee.</value>
-        public decimal TransactionFee { get; set; }
-        /// <summary>
         /// Gets or sets the merchant order identifier.
         /// </summary>
         /// <value>The merchant order identifier.</value>
@@ -56,7 +46,7 @@ namespace YenePaySdk
         /// <summary>
         /// description of the payment status
         /// </summary>
-        public string StatusDescription { get; set; }
+        public string TransactionCode { get; set; }
         /// <summary>
         /// Gets or sets the currency.
         /// </summary>
@@ -74,14 +64,12 @@ namespace YenePaySdk
             Dictionary<string, string> keyValues = new Dictionary<string, string>();
             keyValues.Add("TotalAmount", this.TotalAmount.ToString());
             keyValues.Add("BuyerId", this.BuyerId.ToString());
-            keyValues.Add("BuyerName", this.BuyerName);
-            keyValues.Add("TransactionFee", this.TransactionFee.ToString());
             keyValues.Add("MerchantOrderId", this.MerchantOrderId.ToString());
             keyValues.Add("MerchantId", this.MerchantId.ToString());
             keyValues.Add("MerchantCode", this.MerchantCode);
             keyValues.Add("TransactionId", this.TransactionId.ToString());
             keyValues.Add("Status", this.Status);
-            keyValues.Add("StatusDescription", this.StatusDescription);
+            keyValues.Add("TransactionCode", this.TransactionCode);
             keyValues.Add("Currency", this.Currency);
             keyValues.Add("Signature", this.Signature);
             return keyValues;
